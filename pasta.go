b02b2358155ac10377625f740c80a459d51a3bc1
Neel Jha <neel.jha@gatech.edu>
2018-12-12 10:21:23 -0500

    Structure and logic for list

--- pasta.go
+++ pasta.go
@@ -34,6 +34,12 @@ type PastaKey struct {
 	Alias string `dynamodbav:"alias"`
 }
 
+type PastaPage struct {
+	Guild string `dynamodbav:"guild"`
+	Alias string `dynamodbav:"alias"`
+	Key   string `dynamodbav:"marker"`
+}
+
 func NewPastaClient(discordSession *discordgo.Session, dynamoClient *dynamodb.DynamoDB) *PastaClient {
 	return &PastaClient{
 		DiscordSession:     discordSession,
@@ -82,8 +88,59 @@ func (pastaClient *PastaClient) SavePasta(guildID, channelID, owner, alias, past
 	pastaClient.DiscordSession.ChannelMessageSend(channelID, "Copypasta with alias "+alias+" saved.")
 }
 
-func (pastaClient *PastaClient) ListPasta(guildID, channelID, userID string) {
-	return
+func (pastaClient *PastaClient) ListPasta(guildID, channelID string) {
+	start, err := pastaClient.DynamoClient.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(pastaTableName),
+		Key:       buildPastaKey(guildID, "Exclusive Start Key"),
+	})
+	if err != nil {
+		pastaClient.DiscordSession.ChannelMessageSend(channelID, "An error occured. Please try again later.")
+		pastaClient.PastaErrorLogger.Println(err)
+		return
+	}
+	last := start.Item["marker"]
+	// pastaClient.PastaServiceLogger.Printf("Pagination Key=%v\n", page)
+	// var result *dynamodb.QueryOutput
+	pastaList := &dynamodb.QueryInput{
+		TableName: aws.String(pastaTableName),
+		KeyConditionExpression: aws.String("guild=:g AND alias NOT a"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":g": &dynamodb.AttributeValue{
+				S: aws.String(guildID),
+			},
+			":a": &dynamodb.AttributeValue{
+				S: aws.String("Exclusive Start Key"),
+			},
+		},
+		Limit: aws.Int64(1),
+	}
+	if last != nil {
+		pastaList.ExclusiveStartKey = buildPastaKey(guildID, *last.S)
+	}
+	pastaClient.PastaServiceLogger.Printf("Pasta List Request=%v\n", pastaList)
+	result, err := pastaClient.DynamoClient.Query(pastaList)
+	if err != nil {
+		pastaClient.PastaErrorLogger.Println(err)
+		return	
+	}
+	pastaClient.PastaServiceLogger.Printf("Pasta List Result=%v\n", result)
+	//Save the page for later
+	if result.LastEvaluatedKey != nil {
+		page := *result.LastEvaluatedKey["alias"].S
+		pastaClient.DynamoClient.PutItem(&dynamodb.PutItemInput{
+			TableName:           aws.String(pastaTableName),
+			Item:                buildPastaPage(guildID, page),
+		})
+	} else {
+		_, err := pastaClient.DynamoClient.DeleteItem(&dynamodb.DeleteItemInput{
+			TableName: aws.String(pastaTableName),
+			Key:       buildPastaKey(guildID, "Exclusive Start Key"),
+		})
+		if err != nil {
+			pastaClient.PastaErrorLogger.Println(err)
+		}
+	}
+	pastaClient.DiscordSession.ChannelMessageSend(channelID, result.GoString())
 }
 
 func buildPastaKey(guildID, alias string) map[string]*dynamodb.AttributeValue {
@@ -105,5 +162,15 @@ func buildPasta(guildID, owner, alias, pasta string) map[string]*dynamodb.Attrib
 	}
 	//err != nil will get caught in the request
 	item, _ := dynamodbattribute.MarshalMap(pastaItem)
+	return item
+}
+
+func buildPastaPage(guildID, alias string) map[string]*dynamodb.AttributeValue {
+	page := PastaPage{
+		Guild: guildID,
+		Alias: "Exclusive Start Key",
+		Key: alias,
+	}
+	item, _ := dynamodbattribute.MarshalMap(page)
 	return item
 }
