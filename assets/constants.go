9d5d61f0ae62c82aa3f51fb6e24f9cac85602d80
Neel Jha <neel.jha@gatech.edu>
2018-12-20 02:51:14 -0500

    Centralizing the safe-to-commit constants.

---
+++ assets/constants.go
@@ -0,0 +1,13 @@
+package assets
+
+// A collection of project-wide constants all in one place
+const (
+	// AvatarURL is a link to the profile picture used in embed on behalf of Flamingo
+	AvatarURL = "https://s3.amazonaws.com/flamingo-bot/pfp"
+	// BucketName is the S3 bucket where Flamingo stores assets
+	BucketName = "flamingo-bot"
+	// StrikeTableName is the name of the table where strikes are persisted
+	StrikeTableName = "FlamingoStrikes"
+	// PastaTableName is the name of the table where pastas are persisted
+	PastaTableName = "FlamingoPasta"
+)
